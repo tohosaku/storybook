86b5b788c2d8214c82f1787b096ff45d413e7eaf
Takashi Kato <tohosaku@users.osdn.me>
2023-10-06 00:52:58 +0900

    Upgrade to storybook 7.4 and use vite instead of webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,25 @@
-module.exports = {
+const config = {
   "stories": [
-    "../src/stories/**/*.stories.mdx",
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-viewport",
+    "@storybook/addon-interactions"
   ],
+
   staticDirs: [
     '../public'
   ],
-  "framework": "@storybook/html",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+
+  "framework": {
+    name: "@storybook/html-vite"
   },
+
+  docs: {
+    autodocs: true
+  }
 }
+
+export default config
